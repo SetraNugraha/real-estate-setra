50166ec967174b441ff61ea2640896a65b5b836b
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2025-04-20 14:34:44 +0700

    add: add dummy data for properties section

---
+++ src/dummyData/properties.ts
@@ -0,0 +1,82 @@
+type Facilities = {
+  beds: number
+  bath: number
+}
+
+type Tags = {
+  name: string
+  image: string | File
+  color: string
+}
+
+interface Properties {
+  id: number
+  name: string
+  address: string
+  tags?: Tags
+  propertieImage: string | File
+  price: number
+  facilities: Facilities
+}
+
+export const properties: Properties[] = [
+  {
+    id: 1,
+    name: 'Tranquil Haven in the Woods',
+    address: '103 Wright CourtBurien, WA 98168',
+    propertieImage: 'tranquil.png',
+    price: 5970,
+    tags: {
+      name: 'popular',
+      image: 'popular.svg',
+      color: 'FFE1E1',
+    },
+    facilities: {
+      beds: 4,
+      bath: 3,
+    },
+  },
+  {
+    id: 2,
+    name: 'Serene Retreat by the Lake',
+    address: '1964 Jehovah Drive, VA 22408',
+    propertieImage: 'serene.png',
+    price: 1970,
+    tags: {
+      name: 'New Listing',
+      image: 'new-listing.svg',
+      color: 'D7EEFF',
+    },
+    facilities: {
+      beds: 3,
+      bath: 2,
+    },
+  },
+  {
+    id: 3,
+    name: 'Charming Cottage in the Meadow',
+    address: '1508 Centennial Farm RoadHarlan, 51537',
+    propertieImage: 'charming.png',
+    price: 3450,
+    tags: {
+      name: 'Dsicounted Price',
+      image: 'discounted.svg',
+      color: 'F1FFF1',
+    },
+    facilities: {
+      beds: 4,
+      bath: 4,
+    },
+  },
+  {
+    id: 4,
+    name: 'Grand Estate on the Hilltop',
+    address: '103 Wright CourtBurien, WA 98168',
+    propertieImage: 'grand.png',
+    price: 2389,
+    facilities: {
+      beds: 4,
+      bath: 3,
+    },
+  },
+]
